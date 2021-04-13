b73da71d28e275aef0d9b89ddfac836276d9c090
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2021-04-14 00:02:54 +0530

    Create EvenOddRollNumbers.c

---
+++ CCIP/EvenOddRollNumbers.c
@@ -0,0 +1,26 @@
+/*
+In a class there can be ’n’ students, whose roll numbers starts from zero ’0’. Two practicals assigned to all the students such that all the students with even roll number will do practical 1 and with odd roll numbers do practical 2.
+
+Marks alloted to each student and store in the form of array. We need to find the absolute difference between the average marks of students with even roll number & odd roll numbers.
+
+Input Format
+
+Accept marks of ‘n’ students.
+
+Constraints
+
+1 <= n <= 50 1 <= marks <= 100
+
+Output Format
+
+Print the absolute value of difference of averages
+
+Sample Input 0
+
+5
+25 36 48 29 95
+Sample Output 0
+
+23.5
+*/
+
