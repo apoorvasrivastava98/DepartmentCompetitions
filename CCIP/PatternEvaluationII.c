e6a6c9f10d5f969e29c6be115c0c3b6d32bf484a
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2021-05-26 20:52:32 +0530

    Create PatternEvaluationII.c

---
+++ CCIP/PatternEvaluationII.c
@@ -0,0 +1,45 @@
+/*
+Print the Pattern as given in sample cases.
+
+Input Format
+
+Enter the value ‘n’.
+
+Constraints
+
+1<= n <= 10
+
+Output Format
+
+Print respective pattern.
+
+Sample Input 0
+
+3
+Sample Output 0
+
+A
+AB
+ABC
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%c",(char)(j+64));
+        }
+        printf("\n");
+    }
+    return 0;
+}
